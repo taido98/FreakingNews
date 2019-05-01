66caf3b6a7e978000525b19d92f5f64e9f580dca
unknown <you@example.com>
2019-05-02 01:39:23 +0700

    merge

--- app/src/main/java/com/example/naviapplication/Main2Activity.java
+++ app/src/main/java/com/example/naviapplication/Main2Activity.java
@@ -3,20 +3,31 @@ package com.example.naviapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class Main2Activity extends AppCompatActivity {
-
     WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         webView = (WebView) findViewById(R.id.webView);
         Intent intent = getIntent();
         String linkPath = intent.getStringExtra("link");
         webView.loadUrl(linkPath);
         webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home : onBackPressed();
+            return true;
+            default:break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
