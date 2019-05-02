383570b882f18419a1b99bf34505f2fb125ccf92
unknown <you@example.com>
2019-05-02 23:16:14 +0700

    add_post

--- app/src/main/java/com/example/naviapplication/AddPostActivity.java
+++ app/src/main/java/com/example/naviapplication/AddPostActivity.java
@@ -1,18 +1,36 @@
 package com.example.naviapplication;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AddPostActivity extends AppCompatActivity {
 
     private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +41,81 @@ public class AddPostActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.custom_addpost_actionbar);
 
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        String im_url = "http://sohanews.sohacdn.com/thumb_w/660/2018/8/28/photo1535416480861-15354164808651145995032.png";
+//        String im_url = "http://sohanews.sohacdn.com/thumb_w/660/2018/8/28/photo1535416480861-15354164808651145995032.png";
+        String im_url = "https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.0-9/58595586_2335381520025428_7214766066176622592_n.jpg?_nc_cat=108&_nc_oc=AQmkxqamW6GnIkygV8Hd9CHkvPMyTiTnrTJG_nXym340BAOyvm6cM1Y6w7nmnYGfz3U&_nc_ht=scontent.fsgn2-3.fna&oh=86463bbafdd68f9cda8a50e0da8f7370&oe=5D662368";
         imageView = findViewById(R.id.avatar_post);
 
         Picasso.with(this).load(im_url).into(imageView);
+
+        Button picker = (Button) findViewById(R.id.picker);
+        picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(Intent.createChooser(intent, "Chọn ảnh"), 100);
+
+                Intent intent = new Intent();
+                // Show only images, no videos or anything else
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                // Always show the chooser (if there are multiple options available)
+                startActivityForResult(Intent.createChooser(intent, "Chọn ảnh"), 100);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                //Lấy dữ liệu dạng bitmap
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                //Scale ảnh theo màn hình
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int width = displayMetrics.widthPixels;
+                int height = bitmap.getHeight()*width/bitmap.getWidth();
+                Toast.makeText(this,width+"x"+height,Toast.LENGTH_LONG).show();
+                Bitmap resize = Bitmap.createScaledBitmap(bitmap,width, height,true);
+
+                //Load ảnh vào imageView
+                ImageView imagePin = (ImageView) findViewById(R.id.image_pin);
+                imagePin.setImageBitmap(resize);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String[] projection = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(projection[0]);
+            String picturePath = cursor.getString(columnIndex); // returns null
+//            Log.d("Duong dan", picturePath);
+            cursor.close();
+        }
+    }
+//
+//    public String getPathFromURI(Uri contentUri) {
+//        String res = null;
+//        String[] proj = {MediaStore.Images.Media.DATA};
+//        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+//        if (cursor.moveToFirst()) {
+//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//            res = cursor.getString(column_index);
+//        }
+//        cursor.close();
+//        return res;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/example/naviapplication/Main2Activity.java
+++ app/src/main/java/com/example/naviapplication/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.example.naviapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -8,17 +9,31 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class Main2Activity extends AppCompatActivity {
-    WebView webView;
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        webView = (WebView) findViewById(R.id.webView);
+
         Intent intent = getIntent();
         String linkPath = intent.getStringExtra("link");
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        //enable javascript engine
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:(function() { " +
+                        "alert('aaaa'); })()");
+            }
+        });
+
         webView.loadUrl(linkPath);
-        webView.setWebViewClient(new WebViewClient());
     }
 
     @Override
