39323c39f3eb6d051d06bbc5c4a132c41a1272a1
unknown <you@example.com>
2019-05-06 10:20:55 +0700

    Do oc cho:

---
+++ app/src/main/java/com/example/naviapplication/fagments/fblogin.java
@@ -0,0 +1,4 @@
+package com.example.naviapplication.fagments;
+
+public class fblogin {
+}
--- app/src/main/java/com/example/naviapplication/ip.java
+++ app/src/main/java/com/example/naviapplication/ip.java
@@ -1,7 +1,7 @@
 package com.example.naviapplication;
 
 public class ip {
-    String ip = "192.168.0.109";
+    String ip = "192.168.43.219";
 
     public String getIp() {
         return ip;
